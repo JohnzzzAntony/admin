898f9263edfe881641ba6ecd50d81e5ee8d6064a
JohnzzzAntony <johnsjoantony@gmail.com>
2026-04-05 00:33:16 +0400

    Deployment: Switched WhiteNoise storage

--- jkr/settings.py
+++ jkr/settings.py
@@ -195,7 +195,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise storage to handle static files in production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
